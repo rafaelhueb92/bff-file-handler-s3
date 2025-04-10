2e979172f7e3b7dff68dba87794b9bf0f5e07194
Rafael Hueb <rafaelhueb@Rafaels-MacBook-Air.local>
2025-04-10 12:46:55 -0300

    infra done

--- docker/push-to-ecr.sh
+++ docker/push-to-ecr.sh
@@ -1,12 +1,20 @@
-ACCOUNT_ID=''
-AWS_REGION=''
-APP_REPO_ECR=''
-APP_REPO_ECR_URL=''
+ACCOUNT_ID=$(aws sts get-caller-identity --output text --query Account)
+AWS_REGION=$(aws configure get region)
+APP_REPO_ECR='bff-file-handler'
+APP_REPO_ECR_URL="$ACCOUNT_ID.dkr.ecr.us-east-1.amazonaws.com/$APP_REPO_ECR"
 
-aws ecr get-login-password --region $AWS_REGION | docker login --username AWS --password-stdin $ACCOUNT_ID.dkr.ecr.$ACCOUNT_ID.amazonaws.co
+echo "Building Image"
 
-docker build -t $APP_REPO_ECR:latest .
+DOCKER_BUILDKIT=1 BUILDKIT_PROGRESS=plain docker build --no-cache -t $APP_REPO_ECR:latest .
+
+echo "Login into ECR"
+
+aws ecr get-login-password --region $AWS_REGION | docker login --username AWS --password-stdin $ACCOUNT_ID.dkr.ecr.$AWS_REGION.amazonaws.co
+
+echo "Tagging Image"
 
 docker tag $APP_REPO_ECR:latest $APP_REPO_ECR_URL:latest
+
+echo "Pushing Image
 
 docker push $APP_REPO_ECR_URL:latest
